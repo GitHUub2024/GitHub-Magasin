420445a53272cbd523363399fb538fbea119c8b0
Alexis Pont-Campos <alexis.pont-campos@scantech.com>
2024-10-14 08:39:16 +0200

    commit test 2

---
+++ travail progra.py
@@ -0,0 +1,13 @@
+class client:
+    
+    def __init__(self,id,name,mdp,email,tel):
+        self.id=identifiant
+        self.name=name
+        self.email=email
+        self.password=mdp
+        self.tel_number=tel
+    def faire_commande(self, contenu_panier):
+        
+    list_vente+=[]
+    montant_total=sum()
+    
