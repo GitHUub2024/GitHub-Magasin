d4d7654825609611e4e2e1e7f38d39f17f9412cc
Alexis Pont-Campos <alexis.pont-campos@scantech.com>
2024-10-14 08:36:44 +0200

    commit test 1

---
+++ classe client.py
@@ -0,0 +1,13 @@
+class User:
+    def __init__(self,id,name,password):
+        self.id=id
+        self.name=name
+        self.password=password
+    def check_pwd(self,password):
+        return self.password==password
+
+
+if __name__=="__main__":
+    john=User()
+    john.name="John"
+    print(john.name)
