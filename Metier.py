ada2429efd4cd7e6fe258779613cb7c29e5ce258
szedxwqa <victorien.guyon@protonmail.com>
2025-02-02 12:00:04 +0100

    Finalisations de la Classe Métier.

--- Metier.py
+++ Metier.py
@@ -3,8 +3,8 @@ Fichier contenant la classe Metier.
 Classe utilitaire du projet de simulation du programme de simulation de fonctionnement de magasin.
 
 Contient les méthodes suivantes :
-    1) Méthodes renvoyant le montant total par rayons et par secteurs d'activité.
-    2) Méthode renvoyant le montant total d'un vendeur
+    1) Méthodes renvoyant le montant total et moyen par rayons et par secteurs d'activité.
+    2) Méthode renvoyant le montant moyen d'une vente d'un vendeur
     3) Méthode renvoyant le montant moyen d'achat par client
 
 Pour toutes idées, remarques et signalements, veuillez contacter Victorien Guyon (M2 MMAA Univ-SMB) :
@@ -15,14 +15,8 @@ Pour toutes idées, remarques et signalements, veuillez contacter Victorien Guyo
 # Importation des classes :
 from Client import Client
 from Vendeur import Vendeur
-from Produit import Produit
-# from Rayon import Rayon
+from Rayon import Rayon
 from SecteurActivite import SecteurActivite
-
-
-# Classe Rayon (Provisoire) :
-class Rayon:
-    pass
 
 
 # Classe Métier :
@@ -33,31 +27,69 @@ class Metier:
     """
 
     @staticmethod
-    def montant_total_Rayon(rayon: Rayon):
+    def montant_total_rayon(rayon: Rayon, ttc: bool = True) -> float:
         """
-        Renvoie le montant to total des produits dans un rayon.
+        Renvoie le montant total des produits dans un rayon. Calcul avec les prix TTC par défault, HT
+        si ttc = False.
         :param rayon: Rayon concerné.
+        :param ttc: Indique le type de prix utilisé dans le calcul du montant. True pour TTC et False pour HT.
         :return: Somme totale en euros des produits du rayon.
         """
-        pass
+        if ttc:
+            total = sum(prod.prix_ttc() * rayon.produit[prod] for prod in rayon.produit)
+        else:
+            total = sum(prod.prix_ht * rayon.produit[prod] for prod in rayon.produit)
+        return total
 
     @staticmethod
-    def montant_total_secteur(secteurActivite: SecteurActivite):
+    def montant_moyen_rayon(rayon: Rayon, ttc: bool = True) -> float:
         """
-        Donne le montant total des produits contenus dans tout un secteur.
+        Renvoie le montant moyen d'un produit dans un rayon. Calcul avec les prix TTC par défault, HT
+        si ttc = False.
+        :param rayon: Rayon en question.
+        :param ttc: Indique le type de prix utilisé dans le calcul du montant. True pour TTC et False pour HT.
+        :return: Montant moyen sur le rayon.
+        """
+        if ttc:
+            total = sum(prod.prix_ttc() * rayon.produit[prod] for prod in rayon.produit)
+        else:
+            total = sum(prod.prix_ht * rayon.produit[prod] for prod in rayon.produit)
+        return total / sum(rayon.produit[prod] for prod in rayon.produit)
+
+    @staticmethod
+    def montant_total_secteur_ht(secteurActivite: SecteurActivite) -> float:
+        """
+        Donne le montant total hors-taxes des produits contenus dans tout un secteur.
         :param secteurActivite: Secteur en question.
-        :return: Montant total en euros des produits du secteur.
+        :return: Montant total en euros des produits hors_taxes du secteur.
         """
-        pass
+        return sum(prod.prix_ht * prod.stock for prod in secteurActivite.liste_produits)
 
     @staticmethod
-    def montant_total_vendeur(vendeur: Vendeur):
+    def montant_moyen_secteur(secteurActivite: SecteurActivite, ttc: bool = True) -> float:
         """
-        Renvoie le montant total vendu par un vendeur.
+        Renvoie le montant moyen d'un produit d'un secteur d'activité. Calcul avec les prix TTC par défault, HT
+        si ttc = False.
+        :param secteurActivite: Secteur en question.
+        :param ttc: Indique le type de prix utilisé dans le calcul du montant. True pour TTC et False pour HT.
+        :return: Montant moyen sur le secteur.
+        """
+        # Montant total secteur :
+        if ttc:
+            total = sum(prod.prix_ttc() * prod.stock for prod in secteurActivite.liste_produits)
+        else:
+            total = sum(prod.prix_ht * prod.stock for prod in secteurActivite.liste_produits)
+
+        return total / sum(prod.stock for prod in secteurActivite.liste_produits)
+
+    @staticmethod
+    def montant_moyen_vendeur(vendeur: Vendeur) -> float:
+        """
+        Renvoie le montant moyen vendu par un vendeur.
         :param vendeur: Le vendeur concerné.
-        :return: Montant total de produit vendu par le vendeur, en euros.
+        :return: Montant moyen de produit vendu par le vendeur, en euros.
         """
-        pass
+        return sum(vente.prix_total for vente in vendeur.ventes) / len(vendeur.ventes)
 
     @staticmethod
     def montant_moyen_achat_client(client: Client) -> float:
@@ -66,4 +98,4 @@ class Metier:
         :param client: Le client en question.
         :return: Le montant moyen d'achat du client en euros.
         """
-        return sum(vente.prix for vente in client.historique_achats) / len(client.historique_achats)
+        return sum(vente.prix_total for vente in client.historique_achats) / len(client.historique_achats)
